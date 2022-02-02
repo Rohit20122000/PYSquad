e307a0d00846c88e9eb61da2d19f71f33ba5e32e
Rohit20122000 <rrchaudhary99@gmail.com>
2022-02-02 12:34:42 +0530

    cart chages

--- efarming/cart/models.py
+++ efarming/cart/models.py
@@ -3,14 +3,18 @@ from operator import mod
 from django.db import models
 
 # Create your models here.
+
+class Cart(models.Model):
+    customer_name = models.CharField(max_length=50)   
+    no_of_product = models.IntegerField()
+    add_date = models.DateField(null=True,blank=True)
+    total_price = models.DecimalField(max_digits=10,decimal_places=2)
+
 class Order(models.Model):
+    customer_name = models.CharField(max_length=50)
     address = models.TextField()
     details = models.TextField()
     price = models.DecimalField(max_digits=10,decimal_places=2)
     deliveryDate = models.DateField()
     productQuntity = models.IntegerField()
 
-class Cart(models.Model):
-    no_of_product = models.IntegerField()
-    add_date = models.DateField(null=True,blank=True)
-    total_price = models.DecimalField(max_digits=10,decimal_places=2)
