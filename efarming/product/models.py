2ff1863ec667ccff525c4c6c95270c9e3f181395
Rohit20122000 <rrchaudhary99@gmail.com>
2022-02-02 12:09:17 +0530

    remove un. file and model name changes

--- efarming/product/models.py
+++ efarming/product/models.py
@@ -8,3 +8,6 @@ class Product(models.Model):
     price = models.IntegerField(max_length=100)
     description = models.TextField()
     productQuntity = models.IntegerField()
+
+    def __str__(self):
+        return  f"{self.name}"
