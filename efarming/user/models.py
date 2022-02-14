dcc4cb56aec8af9b3da51d434b676e29eb284b46
jemish <love@gmail.com>
2022-02-14 14:25:32 +0530

    changes

--- efarming/user/models.py
+++ efarming/user/models.py
@@ -1,32 +1,27 @@
+import code
 from django.db import models
-
-"""USER_CHOICES = (("Vendor","Vendor"),("Customer","Customer"))"""
 
 # Create your models here.
 class user(models.Model):
     user_name = models.CharField(max_length=100)
-    Vendor = models.ForeignKey(max_length=50,on_delete=models.CASCADE)
     user_password = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
     age =  models.IntegerField()
     address = models.TextField()
     contact = models.IntegerField()
-"""    user_field = models.CharField(choices=USER_CHOICES,max_length=8)"""
 
+    def __str__(self):
+        return self.user_name
 
     
 class Vendor(models.Model):
     code = models.IntegerField()
 
-
+    def __str__(self):
+        return self.name
 
 class Customer(models.Model):
     name = models.CharField(max_length=100)
-    password = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
-    age =  models.IntegerField()
-    address = models.TextField()
-    contact = models.IntegerField()
 
     def __str__(self):
         return self.name
