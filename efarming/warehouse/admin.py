c1479c3bdee71a324feeb3454f70ad8591264689
Rohit Chaudhary <rrchaudhary99@gmail.com>
2022-02-17 16:11:49 +0530

    admin

--- efarming/warehouse/admin.py
+++ efarming/warehouse/admin.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from .models import ProductType,Catagory,warehouse,VendorInventory
 
-
+class VendorInventoryAdmin(admin.ModelAdmin):
+    list_display = ('vendor','productname',)
 
 admin.site.register(ProductType)
 admin.site.register(Catagory)
 admin.site.register(warehouse)
-admin.site.register(VendorInventory)
-
+admin.site.register(VendorInventory,VendorInventoryAdmin)
